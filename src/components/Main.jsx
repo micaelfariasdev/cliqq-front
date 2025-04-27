ed8127ff4655f4b3b807b9cd906cb9a8ffdb006e
Micael Farias <micaelfarias.dev@gmail.com>
2025-04-27 15:08:40 -0300

    apartir daqui vou fazer o login e autenticação

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,7 @@ import { useEffect, useState } from 'react'
 
 function Main() {
     const [itens, setItens] = useState([])
-    const hours = new Date().getHours()
-    
+
     useEffect(() => {
         async function carregarItens() {
             const resposta = await fetch('http://localhost:8000/api/photos/')
@@ -15,12 +14,11 @@ function Main() {
         }
         carregarItens()
     }, [])
-    
+
     return (
         <main className=" text-black p-4 border-2 border-gray-200 flex flex-row justify-between items-center bg-white">
             <div id="feed" className="flex flex-col gap-4 w-full justify-center items-center">
                 {itens.map((item) => (
-                    
                     <div className="flex flex-col gap-2 bg-gray-200 p-5 rounded-2xl w-full" id="post">
                         <div className="post-header overflow-hidden rounded-lg h-fit w-full justify-center items-center flex">
                             <img src={`http://localhost:8000/${item.image}`} alt="Post Image" class="post-image" />
@@ -31,7 +29,7 @@ function Main() {
                                 <p>{item.description}</p>
                             </div>
                             <div className="flex flex-row gap-1">
-                                <img className='h-5 rounded-full border-1 border-gray-500 ' src={`http://localhost:8000/${item.photo_perfil}`} alt="" />
+                                <img className='relative translate-y-1 h-5 rounded-full border-1 border-gray-500 ' src={`http://localhost:8000/${item.photo_perfil}`} alt="" />
                                 <p className="font-bold">{item.author}</p>
                             </div>
                             <div className="flex flex-row gap-2 mt-2 text-gray-500">
